cdd039804de5b0af925795182f7d5c763161fdea
BuiKiet <roiyano1utl@gmail.com>
2023-09-18 15:46:16 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import contactsRouter from "./src/app/routes/contact.route";
+import ApiError from "./src/app/api-error";
 
 const app = express();
 
@@ -9,8 +10,16 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to contact book aplication" });
+app.use((req, res, next) => {
+  // Page not found
+  return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+  // Middleware handler public error
+  return res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
+  });
 });
 
 export default app;
